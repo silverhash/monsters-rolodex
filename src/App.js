d2c647c60b7d81e5fc8884708c359222c3f069ca
AkiraaCloud <cloudakiraa@gmail.com>
2020-12-06 00:08:19 +1100

    adding all project files to repository

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import { CardList } from './components/card-list/card-list.component';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { SearchBox } from './components/search-box/search-box.component';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: ''
+    };
+  }
+
+  componentDidMount() {
+    fetch('http://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(json => this.setState({monsters: json}))
+      .catch(error => console.log('There was an error fetching the users '));
+
+  }
+
+  render() {
+    const  { monsters, searchField } = this.state; //destructuring
+    const filteredMonsters = monsters.filter(m => m.name.toLowerCase().includes(searchField.toLowerCase()));
+    return (
+      <div className="App">
+        <h1 className="h1-title">Monsters Rolodex</h1>
+        <SearchBox placeholder="Search Monsters" handleChange= {e => (
+          this.setState({ searchField: e.target.value}))}
+        />
+        <CardList monsters={ filteredMonsters }></CardList>
+      </div>
+    );
+  }
 }
 
 export default App;
